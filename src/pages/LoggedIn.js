0931f7266ce14373a261707142940ef236a5d3b4
Godstime Obasi <techicovery@gmail.com>
2022-03-22 07:20:11 +0100

    syntax error fix - 2

--- src/pages/LoggedIn.js
+++ src/pages/LoggedIn.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import HeroSection from '../components/HeroSection'
-import { Notification } from '../components/Notification'
+import Notification from '../components/Notification'
 import TrendingCard from '../components/TrendingCard'
 
 import { login, logout } from '../utils'
